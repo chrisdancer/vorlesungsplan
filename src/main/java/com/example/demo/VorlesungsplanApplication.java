b0084c227b073e5411ab252324cbaf7783a4c2c7
Schmitt <WN00136865@wgs.wuerth.com>
2020-09-10 17:57:38 +0200

    src folder

--- src/main/java/com/example/demo/VorlesungsplanApplication.java
+++ src/main/java/com/example/demo/VorlesungsplanApplication.java
@@ -1,4 +1,4 @@
-package com.example.vorlesungsplan;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
